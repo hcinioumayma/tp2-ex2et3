6967fdc3dc95d28cf384924ea3e2adbd17e45a8a
hcinioumayma <147883587+hcinioumayma@users.noreply.github.com>
2023-10-20 23:12:48 +0200

    Delete tp2.java directory

--- tp2.java/ex3tp2/TestPersonne.java
+++
@@ -1,9 +0,0 @@
-public class TestPersonne {
-    public static void main(String[] args) {
-        Personne personne=new Personne();
-        personne.setFirstName("oumayma");
-        personne.setName("hcini");
-        personne.setDaeNaiss(27,04,2001);
-        System.out.println(personne.firstName+" "+personne.name+" votre age est:"+personne.ageEn2023());
-    }
-}
